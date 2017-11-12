7ea2d854c631beddd045198c1669fecaad03b8b2
Leon Jacobs <leonja511@gmail.com>
2017-11-12 15:12:42 +0200

    Remove incorrect hypen.

--- template/template.go
+++ template/template.go
@@ -101,7 +101,7 @@ var HTMLContent = `
     </div>
     <div class="navbar navbar-dark bg-dark">
       <div class="container d-flex justify-content-between">
-        <a href="#" class="navbar-brand">go-witness report</a>
+        <a href="#" class="navbar-brand">gowitness report</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader"
           aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
