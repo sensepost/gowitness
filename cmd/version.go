c1fd8bfef2bb0002cbc49c177d476be242db590a
Leon Jacobs <leonja511@gmail.com>
2019-09-20 14:40:34 +0200

    Indicate when a build is a dev build.

--- cmd/version.go
+++ cmd/version.go
@@ -16,6 +16,15 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Prints the version of gowitness",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		if gitHash == "" {
+			gitHash = "dev"
+		}
+
+		if goVer == "" {
+			goVer = "dev"
+		}
+
 		fmt.Printf("gowitness: %s\n", version)
 		fmt.Printf("\ngit hash: %s\ngo version: %s\n", gitHash, goVer)
 	},
