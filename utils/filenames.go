e947ac5229981a49c8973f49d75fa2e78138efcd
Leon Jacobs <leonja511@gmail.com>
2020-09-09 13:20:36 +0200

    Replace /\'s with -\'s as well

--- utils/filenames.go
+++ utils/filenames.go
@@ -11,7 +11,7 @@ func SafeFileName(str string) string {
 	name := strings.ToLower(str)
 	name = strings.Trim(name, " ")
 
-	separators, err := regexp.Compile(`[ &_=+:]`)
+	separators, err := regexp.Compile(`[ &_=+:/]`)
 	if err == nil {
 		name = separators.ReplaceAllString(name, "-")
 	}
