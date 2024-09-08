bcfc80b3d58b0828689d8a76b9c01a81f0d48a65
Leon Jacobs <leonja511@gmail.com>
2024-09-08 13:37:33 +0200

    (feat) add wappalyzer, tls info and more

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -20,39 +20,86 @@ type Result struct {
 	FinalURL       string `json:"finalurl"`
 	ResponseCode   int    `json:"responsecode"`
 	ResponseReason string `json:"responsereason"`
+	Protocol       string `json:"protocol"`
 	ContentLength  int64  `json:"contentlength"`
 	HTML           string `json:"html"`
 	Title          string `json:"title"`
+	PerceptionHash string `json:"perceptionhash"`
 
 	// Name of the screenshot file
 	Filename string `json:"filename"`
+	IsPDF    bool   `json:"ispdf"`
 
 	// Failed flag set if the result should be considered failed
 	Failed       bool   `json:"failed"`
 	FailedReason string `json:"failedreason"`
+
+	TLS          TLS          `json:"tls"`
+	Technologies []Technology `json:"technologies"`
 
 	Headers []Header     `json:"headers"`
 	Network []NetworkLog `json:"network"`
 	Console []ConsoleLog `json:"console"`
 }
 
-func (r *Result) AddHeader(key string, value string) {
-	r.Headers = append(r.Headers, Header{
-		Key:   key,
-		Value: value,
-	})
+func (r *Result) HeaderMap() map[string][]string {
+	headersMap := make(map[string][]string)
+
+	for _, header := range r.Headers {
+		var values []string
+		for _, headerValue := range header.Values {
+			values = append(values, headerValue.Value)
+		}
+		// Append the values to the map for the given header key
+		headersMap[header.Key] = append(headersMap[header.Key], values...)
+	}
+
+	return headersMap
 }
 
-func (r *Result) AddNetworkLog(log NetworkLog) {
-	r.Network = append(r.Network, log)
+type TLS struct {
+	ID       uint `json:"-" gorm:"primarykey"`
+	ResultID uint `json:"-"`
+
+	Protocol                 string       `json:"protocol"`
+	KeyExchange              string       `json:"keyexchange"`
+	Cipher                   string       `json:"cipher"`
+	SubjectName              string       `json:"subjectname"`
+	SanList                  []TLSSanList `json:"sanlist"`
+	Issuer                   string       `json:"issuer"`
+	ValidFrom                float64      `json:"validfrom"`
+	ValidTo                  float64      `json:"validto"`
+	ServerSignatureAlgorithm *int         `json:"serversignaturealgorithm"`
+	EncryptedClientHello     bool         `json:"encryptedclienthello"`
+}
+
+type TLSSanList struct {
+	ID    uint `json:"-" gorm:"primarykey"`
+	TLSID uint `json:"-"`
+
+	Value string `json:"value"`
+}
+
+type Technology struct {
+	ID       uint `json:"-" gorm:"primarykey"`
+	ResultID uint `json:"-"`
+
+	Value string `json:"value"`
 }
 
 type Header struct {
 	ID       uint `json:"-" gorm:"primarykey"`
 	ResultID uint `json:"-"`
 
-	Key   string `json:"key"`
-	Value string `json:"value"`
+	Key    string        `json:"key"`
+	Values []HeaderValue `json:"value"`
+}
+
+type HeaderValue struct {
+	ID       uint `json:"-" gorm:"primarykey"`
+	HeaderID uint `json:"-"`
+
+	Value string `json:"string"`
 }
 
 type NetworkLog struct {
