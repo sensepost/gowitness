3870d06e96e2536a286777a2cb0a0e09f0619671
Leon Jacobs <leonja511@gmail.com>
2024-09-11 20:11:42 +0200

    (feat) mutex results writes

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -211,17 +211,20 @@ func (run *Runner) witness(target string) {
 		result = &models.Result{
 			URL: target,
 		}
-		netlog = make(map[string]models.NetworkLog)
+		resultMutex   = sync.Mutex{}
+		netlog        = make(map[string]models.NetworkLog)
+		dismissEvents = false // set to true to stop EachEvent callbacks
 	)
 
 	go page.EachEvent(
 		// dismiss any javascript dialogs
-		func(e *proto.PageJavascriptDialogOpening) {
+		func(e *proto.PageJavascriptDialogOpening) bool {
 			_ = proto.PageHandleJavaScriptDialog{Accept: true}.Call(page)
+			return dismissEvents
 		},
 
 		// log console.* calls
-		func(e *proto.RuntimeConsoleAPICalled) {
+		func(e *proto.RuntimeConsoleAPICalled) bool {
 			v := ""
 			for _, arg := range e.Args {
 				if !arg.Value.Nil() {
@@ -230,18 +233,22 @@ func (run *Runner) witness(target string) {
 			}
 
 			if v == "" {
-				return
+				return dismissEvents
 			}
 
+			resultMutex.Lock()
 			result.Console = append(result.Console, models.ConsoleLog{
 				Type:  string(e.Type),
 				Value: strings.TrimSpace(v),
 			})
+			resultMutex.Unlock()
+
+			return dismissEvents
 		},
 
 		// network related events
 		// write a request to the network request map
-		func(e *proto.NetworkRequestWillBeSent) {
+		func(e *proto.NetworkRequestWillBeSent) bool {
 			// note the request id for the first request. well get back
 			// to this afterwards to extract information about the probe.
 			if first == "" {
@@ -254,14 +261,17 @@ func (run *Runner) witness(target string) {
 				RequestType: models.HTTP,
 				URL:         e.Request.URL,
 			}
+
+			return dismissEvents
 		},
 
 		// write the response to the network request map
-		func(e *proto.NetworkResponseReceived) {
+		func(e *proto.NetworkResponseReceived) bool {
 			// grab an existing requestid, and add response info
 			if entry, ok := netlog[string(e.RequestID)]; ok {
-				// update the first request details
+				// update the first request details (headers, tls, etc.)
 				if first == string(e.RequestID) {
+					resultMutex.Lock()
 					result.FinalURL = e.Response.URL
 					result.ResponseCode = e.Response.Status
 					result.ResponseReason = e.Response.StatusText
@@ -297,24 +307,33 @@ func (run *Runner) witness(target string) {
 							ServerSignatureAlgorithm: e.Response.SecurityDetails.ServerSignatureAlgorithm,
 							EncryptedClientHello:     e.Response.SecurityDetails.EncryptedClientHello,
 						}
+						resultMutex.Unlock()
 					}
-				} else { // else, add a network log
-					entry.StatusCode = e.Response.Status
-					entry.URL = e.Response.URL
-					entry.RemoteIP = e.Response.RemoteIPAddress
-					entry.MIMEType = e.Response.MIMEType
-					entry.Time = e.Response.ResponseTime.Time()
-
-					// write the network log
-					result.Network = append(result.Network, entry)
 				}
+
+				// logger.Debug("network response", "url", e.Response.URL, "status", e.Response.Status, "mime", e.Response.MIMEType)
+
+				entry.StatusCode = e.Response.Status
+				entry.URL = e.Response.URL
+				entry.RemoteIP = e.Response.RemoteIPAddress
+				entry.MIMEType = e.Response.MIMEType
+				entry.Time = e.Response.ResponseTime.Time()
+
+				// write the network log
+				resultMutex.Lock()
+				result.Network = append(result.Network, entry)
+				resultMutex.Unlock()
 			}
+
+			return dismissEvents
 		},
 
 		// mark a request as failed
-		func(e *proto.NetworkLoadingFailed) {
+		func(e *proto.NetworkLoadingFailed) bool {
 			// grab an existing requestid an add failure info
 			if entry, ok := netlog[string(e.RequestID)]; ok {
+				resultMutex.Lock()
+
 				// update the first request details
 				if first == string(e.RequestID) {
 					result.Failed = true
@@ -325,7 +344,11 @@ func (run *Runner) witness(target string) {
 					// write the network log
 					result.Network = append(result.Network, entry)
 				}
+
+				resultMutex.Unlock()
 			}
+
+			return dismissEvents
 		},
 
 		// TODO: wss
@@ -339,11 +362,8 @@ func (run *Runner) witness(target string) {
 		return
 	}
 
-	// wait for the --delay number of seconds
-	page.Timeout(time.Duration(run.options.Scan.Delay) * time.Second).WaitNavigation(proto.PageLifecycleEventNameFirstMeaningfulPaint)()
-
 	// wait for navigation to complete
-	if err := page.WaitLoad(); err != nil {
+	if err := page.WaitDOMStable(time.Duration(run.options.Scan.Delay)*time.Second, 0); err != nil {
 		if run.options.Logging.LogScanErrors {
 			logger.Error("could not wait for window.onload", "err", err)
 		}
@@ -360,7 +380,7 @@ func (run *Runner) witness(target string) {
 	if run.options.Scan.JavaScript != "" {
 		_, err := page.Eval(run.options.Scan.JavaScript)
 		if err != nil {
-			log.Warn("failed to evaluate user-provided javascript", "err", err)
+			logger.Warn("failed to evaluate user-provided javascript", "err", err)
 		}
 	}
 
@@ -378,6 +398,9 @@ func (run *Runner) witness(target string) {
 	} else {
 		result.HTML = html
 	}
+
+	// stop the event handlers
+	dismissEvents = true
 
 	// fingerprint technologies in the first response
 	if fingerprints := run.wappalyzer.Fingerprint(result.HeaderMap(), []byte(result.HTML)); fingerprints != nil {
@@ -404,34 +427,37 @@ func (run *Runner) witness(target string) {
 		if run.options.Logging.LogScanErrors {
 			logger.Error("could not take screenshot", "err", err)
 		}
-		return
-	}
 
-	// write the screenshot to disk
-	result.Filename = islazy.SafeFileName(target) + "." + run.options.Scan.ScreenshotFormat
-	if err := os.WriteFile(
-		filepath.Join(run.options.Scan.ScreenshotPath, result.Filename),
-		img, 0o664,
-	); err != nil {
-		if run.options.Logging.LogScanErrors {
-			logger.Error("could not write screenshot to disk", "err", err)
+		result.Failed = true
+		result.FailedReason = err.Error()
+	} else {
+
+		// write the screenshot to disk
+		result.Filename = islazy.SafeFileName(target) + "." + run.options.Scan.ScreenshotFormat
+		if err := os.WriteFile(
+			filepath.Join(run.options.Scan.ScreenshotPath, result.Filename),
+			img, 0o664,
+		); err != nil {
+			if run.options.Logging.LogScanErrors {
+				logger.Error("could not write screenshot to disk", "err", err)
+			}
+			return
 		}
-		return
-	}
 
-	// calculate and set the perception hash
-	decoded, _, err := image.Decode(bytes.NewReader(img))
-	if err != nil {
-		logger.Error("failed to decode screenshot image", "err", err)
-		return
-	}
+		// calculate and set the perception hash
+		decoded, _, err := image.Decode(bytes.NewReader(img))
+		if err != nil {
+			logger.Error("failed to decode screenshot image", "err", err)
+			return
+		}
 
-	hash, err := goimagehash.PerceptionHash(decoded)
-	if err != nil {
-		logger.Error("failed to calculate image perception hash", "err", err)
-		return
+		hash, err := goimagehash.PerceptionHash(decoded)
+		if err != nil {
+			logger.Error("failed to calculate image perception hash", "err", err)
+			return
+		}
+		result.PerceptionHash = hash.ToString()
 	}
-	result.PerceptionHash = hash.ToString()
 
 	// we have everything we can enumerate!
 	// pass the result off the configured writers.
