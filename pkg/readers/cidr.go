2338a3d4f08348ad486b4c200da90bcfd2fd762c
Leon Jacobs <leonja511@gmail.com>
2024-09-15 13:46:24 +0200

    (feat) add ports flags to scan file command
    fixes #165

--- pkg/readers/cidr.go
+++ pkg/readers/cidr.go
@@ -26,12 +26,6 @@ type CidrReaderOptions struct {
 	Random      bool
 }
 
-var (
-	small  = []int{8080, 8443}
-	medium = append(small, []int{81, 90, 591, 3000, 3128, 8000, 8008, 8081, 8082, 8834, 8888, 7015, 8800, 8990, 10000}...)
-	large  = append(medium, []int{300, 2082, 2087, 2095, 4243, 4993, 5000, 7000, 7171, 7396, 7474, 8090, 8280, 8880, 9443}...)
-)
-
 func NewCidrReader(opts *CidrReaderOptions) *CidrReader {
 	return &CidrReader{
 		Options: opts,
--- pkg/readers/reader.go
+++ pkg/readers/reader.go
@@ -7,3 +7,10 @@ package readers
 type Reader interface {
 	Read(chan<- string) error
 }
+
+// port collections that readers can refer to
+var (
+	small  = []int{8080, 8443}
+	medium = append(small, []int{81, 90, 591, 3000, 3128, 8000, 8008, 8081, 8082, 8834, 8888, 7015, 8800, 8990, 10000}...)
+	large  = append(medium, []int{300, 2082, 2087, 2095, 4243, 4993, 5000, 7000, 7171, 7396, 7474, 8090, 8280, 8880, 9443}...)
+)
