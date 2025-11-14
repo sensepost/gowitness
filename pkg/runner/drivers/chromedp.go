558940398a9ae42bed7056d38dd8e2f0d97c5e55
Elymaro <124057003+Elymaro@users.noreply.github.com>
2025-11-14 06:06:23 +0100

    Fix "error enabling network tracking: context deadline exceeded" error message (#292)

--- pkg/runner/drivers/chromedp.go
+++ pkg/runner/drivers/chromedp.go
@@ -84,6 +84,8 @@ func getChromedpAllocator(opts runner.Options) (*browserInstance, error) {
 			chromedp.Flag("https-upgrades-enabled", false),
 			chromedp.Flag("disable-features", "HttpsUpgrades"),
 			chromedp.Flag("explicitly-allowed-ports", restrictedPorts()),
+			chromedp.Flag("no-sandbox", true),
+			chromedp.Flag("disable-gpu", true),
 			chromedp.WindowSize(opts.Chrome.WindowX, opts.Chrome.WindowY),
 			chromedp.UserDataDir(userData),
 		)
