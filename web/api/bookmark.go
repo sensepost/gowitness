20ebc3ffac9231ab9d61214eb2cded5e33ce0163
Daylam Tayari <git@tayari.gg>
2025-12-21 13:47:04 -0600

    Updated bookmark API to support removing bookmarks

--- web/api/bookmark.go
+++ web/api/bookmark.go
@@ -11,15 +11,15 @@ type bookmarkRequest struct {
 	ID int `json:"id"`
 }
 
-// BookmarkHandler marks a result as bookmarked.
+// BookmarkHandler inverts the state of a bookmark
 //
-//	@Summary		Bookmark results
-//	@Description	Marks a given result as bookmarked, writing results to the database.
+//	@Summary		Bookmark/Unbookmarks result
+//	@Description	Inverts the bookmark status of a result, writing results to the database.
 //	@Tags			Results
 //	@Accept			json
 //	@Produce		json
 //	@Param			query	body	bookmarkRequest	true	"The bookmark request object"
-//	@Success		200		{string}	string			"ok"
+//	@Success		200		{string}	string			"bookmarked"
 //	@Router			/results/bookmark [post]
 func (h *ApiHandler) BookmarkHandler(w http.ResponseWriter, r *http.Request) {
 	var request bookmarkRequest
@@ -29,14 +29,29 @@ func (h *ApiHandler) BookmarkHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Info("bookmarking id", "id", request.ID)
-
-	if err := h.DB.Update("bookmark", true).Where("id = ?", request.ID).Error; err != nil {
-		log.Error("failed to bookmark result", "err", err)
+	var bookmarked bool
+	var err error
+	bookmarkResult := h.DB.Select("bookmarked").Where("id = ?", request.ID).First(&bookmarked)
+	if bookmarkResult.Error != nil {
+		log.Error("failed to get bookmark status", "err", bookmarkResult.Error)
+		http.Error(w, "Error getting result bookmark value", http.StatusInternalServerError)
 		return
 	}
 
-	response := `ok`
+	log.Info("inverting bookmark id", "id", request.ID)
+	if err := h.DB.Update("bookmark", !bookmarked).Where("id = ?", request.ID).Error; err != nil {
+		log.Error("failed to update result bookmark", "err", err)
+		http.Error(w, "Error updating result bookmark value", http.StatusInternalServerError)
+		return
+	}
+
+	var response string
+	if bookmarked {
+		response = `removed bookmark`
+	} else {
+		response = `bookmarked`
+	}
+
 	jsonData, err := json.Marshal(response)
 	if err != nil {
 		http.Error(w, "Error creating JSON response", http.StatusInternalServerError)
